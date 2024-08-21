77dedcafe39b124133f75c9dbb2019c44598947f
rodrigo <24rodrigocurso@gmail.com>
2024-08-21 14:35:40 -0300

    Condição ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota =7;
-        if(nota >=7){
-            System.out.println("Aprovado");
-        }else{
-            System.out.println("Reprovado");
-        }
+        String Resultado = nota >=7 ? "aprovado": "reprovado";
+        System.out.println(Resultado);
     }
 }
