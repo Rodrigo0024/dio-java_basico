0a7d2cdd82ed7abf4feb548ee9fd543219aa5299
rodrigo <24rodrigocurso@gmail.com>
2024-08-21 15:25:01 -0300

    Primeira forma de imprimir todos os dados do array

---
+++ controle-fluxo/src/ExemploFor2.java
@@ -0,0 +1,9 @@
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        String alunos[] = {"Rafaelle", "Rodrigo", "Letcia correa"};
+
+        for (int x=0; x <alunos.length; x++){
+            System.out.println("O aluno do indice  " + x + " é "+ alunos[x]);
+    }
+    }
+}
