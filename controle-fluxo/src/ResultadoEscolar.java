5abf2277befebe8fc8bd0592ee906a44d7be123e
rodrigo <24rodrigocurso@gmail.com>
2024-08-21 14:40:43 -0300

    Operador ternario

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota =7;
-        String Resultado = nota >=7 ? "aprovado": "reprovado";
+        String Resultado = nota >=7 ? "aprovado" : nota>=5 && nota <7 ? "Recuperação":"Reprovado" ;
         System.out.println(Resultado);
     }
 }
